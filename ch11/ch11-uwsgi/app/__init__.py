e3097e95f03c96d0bb5e3621b649e536fc06fd0e
sjctrags <sjctrags@yahoo.com>
2024-06-20 21:20:11 +0800

    Update __init__.py
    Editing the code.

--- ch11/ch11-uwsgi/app/__init__.py
+++ ch11/ch11-uwsgi/app/__init__.py
@@ -1,15 +1,11 @@
 from flask import Flask
 import toml
 
-
 def create_app(config_file):
     app = Flask(__name__)
     app.config.from_file(config_file, toml.load)
    
-
-    with app.app_context():  
-        
-        
+    with app.app_context():          
         from app.users import users_bp
         from app.product import product_bp
         from app.login import login_bp
@@ -17,6 +13,5 @@ def create_app(config_file):
         app.register_blueprint(product_bp, url_prefix='/ch11')
         app.register_blueprint(users_bp, url_prefix='/ch11')
         app.register_blueprint(login_bp, url_prefix='/ch11')
-        
         
     return app
